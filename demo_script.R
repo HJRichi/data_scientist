62f65af1ee2d81e4c744a1b82f015136d029f8a5
HJRichi <wissens.business@gmail.com>
2024-03-25 18:08:42 +0100

    Eine Testdatei zum testen von Git und Github

---
+++ demo_script.R
@@ -0,0 +1,9 @@
+# Demo R Script für das Projekt data_scientist
+
+# Erstelle eine Sequenz von Zahlen und berechne deren Quadratwerte
+zahlen <- 1:10
+quadrate <- zahlen^2
+
+# Drucke die Ergebnisse
+print(data.frame(zahlen, quadrate))
+
